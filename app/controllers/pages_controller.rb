c601880c83902a60692559f90acc3cc64cd83020
Mike Kauffman <mkauffman@quickleft.com>
2015-05-16 21:19:27 -0600

    Adding welcome text and background image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+
+  WELCOME_LANGUAGES = ["Welcome", "Maeva", "Welkom", "Kushe", "Weizo", "Benvenuti", "Cao mung", "Witamy", "Mire se", "Vini", "Vitaite", "Sewa", "Merhaba", "Shagatom", "Kiriniz", "Foon Ying", "Keblar"]
+
   def home
+    @welcome_languages = WELCOME_LANGUAGES
   end
+
 end
