712f5e9b127ed16cbeabfc228659e24bccd7ea54
Mike Kauffman <mkauffman@quickleft.com>
2015-05-19 10:17:28 -0600

    Changing email addresses

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,16 +2,16 @@ class Notifier < ApplicationMailer
 
   def bike_request(hashed_params)
     @user_details = hashed_params
-    mail(to: "mike.a.kauffman@gmail.com", subject: "New Bike Request")
+    mail(to: "bicycles4students@gmail.com", subject: "New Bike Request")
   end
 
   def newsletter(hashed_params)
     @user_details = hashed_params
-    mail(to: "mike.a.kauffman@gmail.com", subject: "Newsletter Signup")
+    mail(to: "j1rehoboth@gmail.com", subject: "Newsletter Signup")
   end
 
   def repair_request(hashed_params)
     @user_details = hashed_params
-    mail(to: "mike.a.kauffman@gmail.com", subject: "New Repair Request")
+    mail(to: "bicycles4students@gmail.com", subject: "New Repair Request")
   end
 end
