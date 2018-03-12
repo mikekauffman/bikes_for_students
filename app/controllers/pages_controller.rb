f9469bdd2eace06bf23aa3fb9789c7038d83c3fb
Mike Kauffman <mkauffman@quickleft.com>
2018-03-12 14:18:44 -0400

    removing temp messaging for staging

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,8 @@ class PagesController < ApplicationController
   WELCOME_LANGUAGES = ["Welcome", "Dobrodošli","Maeva", "Welkom", "Kushe", "Weizo", "Benvenuti", "Cao mung", "Witamy", "Mire se", "Vini", "Vitaite", "Sewa", "Merhaba", "Shagatom", "Kiriniz", "Foon Ying", "Keblar"]
 
   def home
-    # @welcome_languages = WELCOME_LANGUAGES
-    render :temp
+    @welcome_languages = WELCOME_LANGUAGES
+    # render :temp
   end
 
   def newsletter
